519b237b15d50e74f07e6734d7afd2a91d7708ec
Tyler Jones <jonesty@os2.engr.oregonstate.edu>
2017-10-22 15:56:13 -0700

    First commit for conc2. Creating functions and pseudocode for forks/chopsticks

--- conc2/conc2.cpp
+++ conc2/conc2.cpp
@@ -1,8 +1,59 @@
 #include<iostream>
+#include<pthread.h>
+#include<stdoio.h>
+#include<stdlib.h>
+#include<semaphore.h>
+#include<time.h>
+
+
+
 using namespace std;
 
+sem_t f1,f2,f3,f4,f5;
+
+sem_init(&f1,0,1);
+sem_init(&f2,0,1);
+sem_init(&f3,0,1);
+sem_init(&f4,0,1);
+sem_init(&f5,0,1);
+
+sem_t forks [] = {f1, f2, f3, f4, f5};
+
+int left(i){ 
+   return i;
+}
+
+int right(i){ 
+   return(i+1)%5;
+}
+
+void get_forks_lefty(i){
+	forks[left(i)].wait();
+	forks[right(i)].wait();
+}
+
+void get_forks_righty(i){
+   	forks[right(i)].wait();
+	forks[left(i)].wait();
+}
+
+void put_forks(i){
+   forks[right(i)].signal();
+   forks[left(i)].signal();
+}
+
+void all_they_do_is_eat_and_think(){
+	int x = 1;
+     while(x==1){
+	
+     }	
+}
+
+
 int main(){
-  
+  srand(time(NULL));
+   
+   pthread_t Locke, Socrates, Plato, Confucius, Descartes;
 
   return 0;
 }
