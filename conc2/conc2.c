3ec37cdf0cf74095bd36a48c053386ed14aa1624
Tyler Jones <jonesty@os2.engr.oregonstate.edu>
2017-10-22 18:01:49 -0700

    performed some testing. Working solution implemented. Fine tuning needed.

--- conc2/conc2.c
+++ conc2/conc2.c
@@ -5,12 +5,7 @@
 #include<time.h>
 #include<pthread.h>
 
-sem_t [5] forks;
-
-struct thread_arg{
-   sem_t[5] phils;
-   int position;
-};
+sem_t forks[5];
 
 int left(i){ 
    return i;
@@ -21,41 +16,43 @@ int right(i){
 }
 
 void get_forks_lefty(int i){
-   sem_wait(forks[left(i)]);
-   sem_wait(forks[right(i)]);
+   sem_wait(&forks[left(i)]);
+   sem_wait(&forks[right(i)]);
 }
 
 void get_forks_righty(int i){
-   sem_wait(forks[right(i)]);
-   sem_wait(forks[left(i)]);
+   sem_wait(&forks[right(i)]);
+   sem_wait(&forks[left(i)]);
 }
 
 void put_forks(int i){
-   sem_post(forks[right(i)]);
-   sem_post(forks[left(i)]);
+   sem_post(&forks[right(i)]);
+   sem_post(&forks[left(i)]);
 }
 
-void think(){
+void think(int who){
 	int thinking = 	rand() % 20 + 1;
-	printf("Thinking for %d seconds...", thinking);
-	sleep(thinking);
+	printf("%d: Thinking for %d seconds...\n", who, thinking);
+	sleep(3);
 }
 
-void eat(){
+void eat(int who){
 	int eating = rand() % 9 + 2;
-	printf("Eating for %d seconds...", eating);
+	printf("%d: Eating for %d seconds...\n", who, eating);
 	sleep(eating);
 }
 
 void *all_they_do_is_eat_and_think(void *position){
      int x = 1;
      while(x==1){
-	think();
-        if(position==0)
-	   get_forks_lefty(position);	   
+	think(position);
+        if(position==0){
+	   get_forks_lefty(position);
+           printf("lefty position\n");
+	}	   
 	else
 	   get_forks_righty(position);
-	eat();
+	eat(position);
 	put_forks(position);
      }	
 }
@@ -65,9 +62,6 @@ int main(){
    
    srand(time(NULL));
 
-   struct thread_arg args;
-
-
    sem_t f0,f1,f2,f3,f4;
 
    sem_init(&f0,0,1);
@@ -76,7 +70,11 @@ int main(){
    sem_init(&f3,0,1);
    sem_init(&f4,0,1);
 
-   forks = {f1, f2, f3, f4, f5};
+   forks[0] = f0;
+   forks[1] = f1;
+   forks[2] = f2;
+   forks[3] = f3;
+   forks[4] = f4;
    
    pthread_t Locke, Socrates, Plato, Confucius, Descartes;
    pthread_create(&Locke, NULL, all_they_do_is_eat_and_think, (void*)0);
@@ -86,6 +84,8 @@ int main(){
    pthread_create(&Confucius, NULL, all_they_do_is_eat_and_think, (void*)3);
    pthread_create(&Descartes, NULL, all_they_do_is_eat_and_think, (void*)4);
 
-   pthread_join(&Locke);
+   pthread_join(&Locke, NULL);
+
+   sleep(100000000);
   return 0;
 }
