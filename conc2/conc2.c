69ed076afb7e4c7c810296364bcdc357f18dbf74
Tyler Jones <jonesty@os2.engr.oregonstate.edu>
2017-10-22 17:27:01 -0700

    Changed array of semaphores(forks) global to fix scope issue

--- conc2/conc2.c
+++ conc2/conc2.c
@@ -5,6 +5,8 @@
 #include<time.h>
 #include<pthread.h>
 
+sem_t [5] forks;
+
 struct thread_arg{
    sem_t[5] phils;
    int position;
@@ -18,19 +20,19 @@ int right(i){
    return(i+1)%5;
 }
 
-void get_forks_lefty(int i, sem_t* forks){
-   sem_wait(&forks[left(i)]);
-   sem_wait(&forks[right(i)]);
+void get_forks_lefty(int i){
+   sem_wait(forks[left(i)]);
+   sem_wait(forks[right(i)]);
 }
 
-void get_forks_righty(int i, sem_t* forks){
-   sem_wait(&forks[right(i)]);
-   sem_wait(&forks[left(i)]);
+void get_forks_righty(int i){
+   sem_wait(forks[right(i)]);
+   sem_wait(forks[left(i)]);
 }
 
-void put_forks(int i, sem_t* forks){
-   sem_post(&forks[right(i)]);
-   sem_post(&forks[left(i)]);
+void put_forks(int i){
+   sem_post(forks[right(i)]);
+   sem_post(forks[left(i)]);
 }
 
 void think(){
@@ -45,16 +47,16 @@ void eat(){
 	sleep(eating);
 }
 
-void *all_they_do_is_eat_and_think(void *arguments){
+void *all_they_do_is_eat_and_think(void *position){
      int x = 1;
      while(x==1){
 	think();
         if(position==0)
-	   get_forks_lefty(position, forks);	   
+	   get_forks_lefty(position);	   
 	else
-	   get_forks_righty(position, forks);
+	   get_forks_righty(position);
 	eat();
-	put_forks(position, forks);
+	put_forks(position);
      }	
 }
 
@@ -66,23 +68,23 @@ int main(){
    struct thread_arg args;
 
 
-   sem_t f1,f2,f3,f4,f5;
+   sem_t f0,f1,f2,f3,f4;
 
+   sem_init(&f0,0,1);
    sem_init(&f1,0,1);
    sem_init(&f2,0,1);
    sem_init(&f3,0,1);
    sem_init(&f4,0,1);
-   sem_init(&f5,0,1);
 
-   sem_t forks [] = {f1, f2, f3, f4, f5};
+   forks = {f1, f2, f3, f4, f5};
    
    pthread_t Locke, Socrates, Plato, Confucius, Descartes;
-   pthread_create(&Locke, NULL, all_they_do_is_eat_and_think(0,&forks), 0);
+   pthread_create(&Locke, NULL, all_they_do_is_eat_and_think, (void*)0);
 
-   pthread_create(&Socrates, NULL, all_they_do_is_eat_and_think, 1);
-   pthread_create(&Plato, NULL, all_they_do_is_eat_and_think, 2);
-   pthread_create(&Confucius, NULL, all_they_do_is_eat_and_think, 3);
-   pthread_create(&Descartes, NULL, all_they_do_is_eat_and_think, 4);
+   pthread_create(&Socrates, NULL, all_they_do_is_eat_and_think,(void*)1);
+   pthread_create(&Plato, NULL, all_they_do_is_eat_and_think, (void*)2);
+   pthread_create(&Confucius, NULL, all_they_do_is_eat_and_think, (void*)3);
+   pthread_create(&Descartes, NULL, all_they_do_is_eat_and_think, (void*)4);
 
    pthread_join(&Locke);
   return 0;
