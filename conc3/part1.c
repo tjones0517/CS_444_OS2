4993216e60c2e27957f91cfb7c43777a7b9c331a
Claude Maimon <maimonc@flip3.engr.oregonstate.edu>
2017-11-26 20:26:48 -0800

    finished print statments.

--- conc3/part1.c
+++ conc3/part1.c
@@ -30,8 +30,8 @@ int check_if_3(void *number){
 		
 		if(wait){
 		   waiting += 1;
-		   sleep(2); 
-		   printf("thread %d is waiting \n", thread_num);
+	//	   sleep(2); 
+		 //  printf("thread %d is waiting \n", thread_num);
 		   sem_post(&mutex);
 		   sem_wait(&block);
 		}
@@ -39,7 +39,7 @@ int check_if_3(void *number){
 		else {
 		   active +=1;
 		 // printf("active in else: %d\n", active);
-	//	   printf("thread %d is ACTIVE\n", thread_num);
+		 //  printf("thread %d is ACTIVE\n", thread_num);
 		   if(active == 3){
 			  wait = true;
 		   }
@@ -58,7 +58,7 @@ int check_if_3(void *number){
 		   wait = true;
 		}
 		*/
-		sleep(thread num +2);
+		sleep(2);
 
 	//	printf("thread %d is going to sleep\n", thread_num);
 		sem_wait(&mutex);
@@ -72,6 +72,7 @@ int check_if_3(void *number){
 
 	//	   printf("active after: %d\n", active);
 		if (active ==0){
+		   printf("NUM OF ACTIVE %d\n", active);
 		//printf("thread %d is awake\n", thread_num);
 		//sem_wait(&mutex);
 		//active -=1;
@@ -110,30 +111,31 @@ int main(void){
    int waiting = 0;
    bool wait = false;
 */
+ //  printf("starting program");
    int i;
 
    sem_init(&mutex, 0,1);
-   sem_init(&block, 0, 4);
+   sem_init(&block, 0, 0);
 
    pthread_t pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8;
    pthread_create(&pt1, NULL, check_if_3, (void*)1);
    pthread_create(&pt2, NULL, check_if_3, (void*)2);
    pthread_create(&pt3, NULL, check_if_3, (void*)3);
    pthread_create(&pt4, NULL, check_if_3, (void*)4);
-/*   pthread_create(&pt5, NULL, check_if_3, (void*)5);
+   pthread_create(&pt5, NULL, check_if_3, (void*)5);
    pthread_create(&pt6, NULL, check_if_3, (void*)6);
    pthread_create(&pt7, NULL, check_if_3, (void*)7);
    pthread_create(&pt8, NULL, check_if_3, (void*)8);
-*/
+
    pthread_join(pt1, NULL);
    pthread_join(pt2, NULL);
    pthread_join(pt3, NULL);
    pthread_join(pt4, NULL);
-  /* pthread_join(pt5, NULL);
+   pthread_join(pt5, NULL);
    pthread_join(pt6, NULL);
    pthread_join(pt7, NULL);
    pthread_join(pt8, NULL);
-   */
+   
 return 0;
 
 
